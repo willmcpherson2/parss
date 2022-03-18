0dbdb958aaea3b53ee14f06b6048843fa66e8d57
William McPherson <willmcpherson2@gmail.com>
2022-03-18 16:14:59 +1100

    Add balanced parentheses test

---
+++ test/Balanced.hs
@@ -0,0 +1,69 @@
+module Balanced (tokenTests, parensTests) where
+
+import Combinators
+import Control.Monad.Trans.Class (MonadTrans(lift))
+import Control.Monad.Trans.Maybe (MaybeT(MaybeT, runMaybeT))
+import Harness
+import Parser
+import Test.HUnit (Test)
+import Util (parse)
+
+parensTests :: Test
+parensTests = tests
+  (parse parseTokens)
+  [("parseParens", parseParens)]
+  [ ("()", Parens [])
+  , ("(", ParensErr (Just Open))
+  , (")", ParensErr (Just Close))
+  , ("(())", Parens [Parens []])
+  , ("(a)", Parens [ParensErr (Just UnexpectedToken)])
+  , ("((", ParensErr (Just Open))
+  , ("(()())", Parens [Parens [], Parens []])
+  , ("((()))", Parens [Parens [Parens []]])
+  ]
+
+data Parens
+  = Parens [Parens]
+  | ParensErr (Maybe Token)
+  deriving (Show, Eq)
+
+parseParens :: Parser [Token] Parens
+parseParens =
+  let
+    parens = try $ runMaybeT $ do
+      Open <- MaybeT takeToken
+      parens <- lift $ star $ try $ parens <<|>> tokenErr
+      Close <- MaybeT takeToken
+      pure $ Parens parens
+    tokenErr = try $ runMaybeT $ do
+      UnexpectedToken <- MaybeT takeToken
+      pure $ ParensErr $ Just UnexpectedToken
+    err = ParensErr <$> takeToken
+  in parens <<|>> tokenErr |>> err
+
+tokenTests :: Test
+tokenTests = tests
+  id
+  [("parseTokens", parseTokens)]
+  [("()", [Open, Close]), ("a", [UnexpectedToken])]
+
+data Token
+  = Open
+  | Close
+  | UnexpectedToken
+  deriving (Show, Eq)
+
+parseTokens :: Parser String [Token]
+parseTokens =
+  let
+    open = runMaybeT $ do
+      MaybeT $ try $ match '('
+      pure Open
+    close = runMaybeT $ do
+      MaybeT $ try $ match ')'
+      pure Close
+    err = runMaybeT $ do
+      MaybeT takeToken
+      pure UnexpectedToken
+    token = open <<|>> close <<|>> err
+  in star token
--- test/Main.hs
+++ test/Main.hs
@@ -1,10 +1,17 @@
 module Main (main) where
 
+import qualified Balanced
 import qualified Not
 import System.Exit
 import Test.HUnit (Counts(errors, failures), Test(TestList), runTestTT)
 
 main :: IO ()
 main = do
-  counts <- runTestTT $ TestList [Not.tokenTests, Not.treeTests, Not.exprTests]
+  counts <- runTestTT $ TestList
+    [ Not.tokenTests
+    , Not.treeTests
+    , Not.exprTests
+    , Balanced.tokenTests
+    , Balanced.parensTests
+    ]
   if errors counts + failures counts == 0 then exitSuccess else exitFailure
