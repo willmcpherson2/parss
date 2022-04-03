f6089516d2136171be39f0ef65837807ef0f81d5
William McPherson <willmcpherson2@gmail.com>
2022-04-03 18:56:46 +1000

    Add Pos type

--- src/Stream.hs
+++ src/Stream.hs
@@ -13,22 +13,24 @@ import Parser
 class Stream s a | s -> a where
   takeToken :: Parser s a
 
-doTakeToken :: (s -> Maybe (a, s)) -> (a -> s -> s) -> Parser (s, s) (Maybe a)
-doTakeToken uncons cons = Parser $ \s@(prev, rest) -> case uncons rest of
-  Just (t, rest) -> ((cons t prev, rest), Just t)
+data Pos s = Pos {prev :: s, rest :: s}
+
+doTakeToken :: (s -> Maybe (a, s)) -> (a -> s -> s) -> Parser (Pos s) (Maybe a)
+doTakeToken uncons cons = Parser $ \s@Pos{prev, rest} -> case uncons rest of
+  Just (t, rest) -> (Pos{prev = cons t prev, rest}, Just t)
   Nothing -> (s, Nothing)
 
-instance Stream ([a], [a]) (Maybe a) where
+instance Stream (Pos [a]) (Maybe a) where
   takeToken = doTakeToken uncons (:)
 
-instance Stream (ST.Text, ST.Text) (Maybe Char) where
+instance Stream (Pos ST.Text) (Maybe Char) where
   takeToken = doTakeToken ST.uncons ST.cons
 
-instance Stream (LT.Text, LT.Text) (Maybe Char) where
+instance Stream (Pos LT.Text) (Maybe Char) where
   takeToken = doTakeToken LT.uncons LT.cons
 
-instance Stream (SB.ByteString, SB.ByteString) (Maybe Word8) where
+instance Stream (Pos SB.ByteString) (Maybe Word8) where
   takeToken = doTakeToken SB.uncons SB.cons
 
-instance Stream (LB.ByteString, LB.ByteString) (Maybe Word8) where
+instance Stream (Pos LB.ByteString) (Maybe Word8) where
   takeToken = doTakeToken LB.uncons LB.cons
