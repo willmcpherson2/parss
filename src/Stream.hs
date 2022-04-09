c50068cef93253e8733b3f12328e059879fef233
William McPherson <willmcpherson2@gmail.com>
2022-04-09 22:29:31 +1000

    Abandon Pos stuff

--- src/Stream.hs
+++ src/Stream.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FunctionalDependencies #-}
 
-module Stream (Stream (..), Pos (..)) where
+module Stream (Stream (..)) where
 
 import Data.ByteString qualified as SB
 import Data.ByteString.Lazy qualified as LB
@@ -13,25 +13,22 @@ import Parser
 class Stream s a | s -> a where
   takeToken :: Parser s a
 
-data Pos s = Pos {prev :: s, rest :: s}
-  deriving (Show)
+doTakeToken :: Enum p => (s -> Maybe (a, s)) -> Parser (p, s) (Maybe a)
+doTakeToken uncons = Parser $ \(pos, s) -> case uncons s of
+  Just (t, ts) -> ((succ pos, ts), Just t)
+  Nothing -> ((pos, s), Nothing)
 
-doTakeToken :: (s -> Maybe (a, s)) -> (a -> s -> s) -> Parser (Pos s) (Maybe a)
-doTakeToken uncons cons = Parser $ \s@Pos{prev, rest} -> case uncons rest of
-  Just (t, rest) -> (Pos{prev = cons t prev, rest}, Just t)
-  Nothing -> (s, Nothing)
+instance Enum p => Stream (p, [a]) (Maybe a) where
+  takeToken = doTakeToken uncons
 
-instance Stream (Pos [a]) (Maybe a) where
-  takeToken = doTakeToken uncons (:)
+instance Enum p => Stream (p, ST.Text) (Maybe Char) where
+  takeToken = doTakeToken ST.uncons
 
-instance Stream (Pos ST.Text) (Maybe Char) where
-  takeToken = doTakeToken ST.uncons ST.cons
+instance Enum p => Stream (p, LT.Text) (Maybe Char) where
+  takeToken = doTakeToken LT.uncons
 
-instance Stream (Pos LT.Text) (Maybe Char) where
-  takeToken = doTakeToken LT.uncons LT.cons
+instance Enum p => Stream (p, SB.ByteString) (Maybe Word8) where
+  takeToken = doTakeToken SB.uncons
 
-instance Stream (Pos SB.ByteString) (Maybe Word8) where
-  takeToken = doTakeToken SB.uncons SB.cons
-
-instance Stream (Pos LB.ByteString) (Maybe Word8) where
-  takeToken = doTakeToken LB.uncons LB.cons
+instance Enum p => Stream (p, LB.ByteString) (Maybe Word8) where
+  takeToken = doTakeToken LB.uncons
