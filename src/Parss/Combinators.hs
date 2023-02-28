f1b8bf4165579e822f4ae4642746a1dc7214fd0b
William McPherson <willmcpherson2@gmail.com>
2023-02-28 15:03:10 +1100

    Document combinators
    Use functional dependencies in Stream
    
    Add instance for regular [a]
    
    Change behaviour of `dry`

--- src/Parss/Combinators.hs
+++ src/Parss/Combinators.hs
@@ -25,20 +25,38 @@ import Parss.Parser
 import Parss.Stream
 import Prelude hiding (or)
 
+-- | Get the source.
+--
+-- >>> parse (is 'a' >> source) ("", "abc")
+-- ("a","bc")
 source :: Monoid m => Parser m s s
 source = C.id
 
+-- | Matched input of parser, and its result.
+--
+-- >>> parse (match $ is 'a') "abc"
+-- ("a",Just 'a')
 match :: Parser m s a -> Parser m s (m, a)
 match p = Parser $ \s ->
   let (m, s', x) = runParser p s
    in (m, s', (m, x))
 
+-- | Left hand side of source, i.e. the previous input.
+--
+-- >>> parse (is 'a' >> left) ("", "abc")
+-- "a"
 left :: Monoid m => Parser m (l, r) l
 left = Parser $ \(l, r) -> (mempty, (l, r), l)
 
+-- | Right hand side of source, i.e. the remaining input.
+--
+-- >>> parse (is 'a' >> right) ("", "abc")
+-- "bc"
 right :: Monoid m => Parser m (l, r) r
 right = Parser $ \(l, r) -> (mempty, (l, r), r)
 
+-- | For a parser returning a result that accepts a location, creates
+-- a parser that supplies the location.
 locate :: Monoid m => Parser m (l, r) ((l, m, r) -> a) -> Parser m (l, r) a
 locate p = do
   l <- left
@@ -46,17 +64,33 @@ locate p = do
   r <- right
   pure $ f (l, m, r)
 
+-- | For a parser returning a result that accepts a location, creates
+-- a parser that supplies the location.
 locateM ::
   (Monoid m, Applicative f) =>
   Parser m (l, r) (f ((l, m, r) -> a)) ->
   Parser m (l, r) (f a)
 locateM = locate . fmap (\f x -> f <*> pure x)
 
-dry :: Parser m s a -> Parser m s a
+-- | Dry run of parser. Restores state.
+--
+-- >>> runParser (dry $ is 'a') "ax"
+-- ("","ax",Just 'a')
+--
+-- >>> runParser (dry $ is 'a') "bx"
+-- ("","bx",Nothing)
+dry :: Monoid m => Parser m s a -> Parser m s a
 dry p = Parser $ \s ->
-  let (m, _, x) = runParser p s
-   in (m, s, x)
+  let (_, _, x) = runParser p s
+   in (mempty, s, x)
 
+-- | Try to run parser. Restore state on failure.
+--
+-- >>> runParser (try $ is 'a') "ax"
+-- ("a","x",Just 'a')
+--
+-- >>> runParser (try $ is 'a') "bx"
+-- ("","bx",Nothing)
 try :: Monoid m => Parser m s (Maybe a) -> Parser m s (Maybe a)
 try p = Parser $ \s ->
   let (m, s', x) = runParser p s
@@ -64,33 +98,65 @@ try p = Parser $ \s ->
         Just x -> (m, s', Just x)
         Nothing -> (mempty, s, Nothing)
 
+-- | Run stream into a function to create a parser.
+--
+-- >>> parse (into $ \x -> if x == 'a' then Just x else Nothing) "a"
+-- Just 'a'
 into :: (Stream m s (n a), Monad n) => (a -> n b) -> Parser m s (n b)
 into f = (>>= f) <$> stream
 
+-- | Create a parser for @Maybe a@ from a predicate @a -> Bool@.
+--
+-- >>> parse (satisfy (== 'a')) "a"
+-- Just 'a'
 satisfy :: Stream m s (Maybe a) => (a -> Bool) -> Parser m s (Maybe a)
 satisfy f = into $ \x -> if f x then Just x else Nothing
 
+-- | Like regular expression @a@.
+--
+-- >>> parse (is 'a') "a"
+-- Just 'a'
 is :: (Stream m s (Maybe a), Eq a) => a -> Parser m s (Maybe a)
 is x = satisfy (== x)
 
+-- | Like regular expression @ab@.
+--
+-- >>> parse (cat [is 'a', is 'b']) "ab"
+-- Just "ab"
 cat :: (Traversable t, Monad m, Monad n) => t (m (n a)) -> m (n (t a))
 cat = fmap sequence . sequence
 
+-- | Like regular expression @ab@.
+--
+-- >>> parse (literal "foo") "foo"
+-- Just "foo"
 literal ::
   (Stream m s (Maybe a), Monoid m, Traversable t, Eq a) =>
   t a ->
   Parser m s (Maybe (t a))
 literal = cat . fmap is
 
+-- | Like regular expression @a*@.
+--
+-- >>> parse (star $ try $ is 'a') "aaab"
+-- "aaa"
 star :: Monad m => m (Maybe a) -> m [a]
 star p =
   p >>= \case
     Just x -> (x :) <$> star p
     Nothing -> pure []
 
+-- | Like regular expression @a+@.
+--
+-- >>> parse (plus $ try $ is 'a') "aaab"
+-- Just ('a' :| "aa")
 plus :: Monad m => m (Maybe a) -> m (Maybe (NonEmpty a))
 plus = fmap nonEmpty . star
 
+-- | Like regular expression @a|b@.
+--
+-- >>> parse (is 'a' `or` is 'b') "a"
+-- Just 'a'
 or ::
   Monoid m =>
   Parser m s (Maybe a) ->
@@ -103,6 +169,10 @@ or p q =
 
 infixl 3 `or`
 
+-- | Try @p@, otherwise fall back to @q@.
+--
+-- >>> parse (is 'a' `or` is 'b' `orElse` pure 'c') "x"
+-- 'c'
 orElse :: Monoid m => Parser m s (Maybe a) -> Parser m s a -> Parser m s a
 orElse p q =
   try p >>= \case
--- src/Parss/Stream.hs
+++ src/Parss/Stream.hs
@@ -1,11 +1,16 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module Parss.Stream (Stream (..)) where
 
 import Parss.Parser (Parser (..))
 
-class Stream m s a where
+class Stream m s a | s -> m a where
   stream :: Parser m s a
+
+instance Stream [a] [a] (Maybe a) where
+  stream = Parser $ \s -> case s of
+    x : xs -> ([x], xs, Just x)
+    [] -> ([], s, Nothing)
 
 instance Stream [a] ([a], [a]) (Maybe a) where
   stream = Parser $ \(l, r) -> case r of
