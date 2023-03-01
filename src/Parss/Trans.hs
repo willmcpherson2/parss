dd8d0bd4954ef7548573d3852ddcdda723ed2af3
William McPherson <willmcpherson2@gmail.com>
2023-03-01 14:05:35 +1100

    Reorganise Parss.Trans
    Use concrete types in signatures
    
    Update documentation
    
    Reorder definitions

--- src/Parss/Trans.hs
+++ src/Parss/Trans.hs
@@ -19,14 +19,6 @@ import Parss.Parser (Parser)
 -- >>> import Parss.Combinators
 -- >>> import Parss.Parser
 
--- | Parser with the power to return early with @Nothing@ and with
--- @Left e@.
-type Fallible e m s = ExceptT e (MaybeT (Parser m s))
-
--- | Run a 'Fallible'.
-fallible :: Fallible a m s a -> Parser m s (Maybe a)
-fallible = fmap (fmap $ either id id) . runMaybeT . runExceptT
-
 -- | Parser that always returns a value, but with the power to return
 -- early with @Left e@.
 type Infallible e m s = ExceptT e (IdentityT (Parser m s))
@@ -35,6 +27,39 @@ type Infallible e m s = ExceptT e (IdentityT (Parser m s))
 infallible :: Infallible a m s a -> Parser m s a
 infallible = fmap (either id id) . runIdentityT . runExceptT
 
+-- | Parser with the power to return early with @Nothing@ and with
+-- @Left e@.
+type Fallible e m s = ExceptT e (MaybeT (Parser m s))
+
+-- | Run a 'Fallible'.
+fallible :: Fallible a m s a -> Parser m s (Maybe a)
+fallible = fmap (fmap $ either id id) . runMaybeT . runExceptT
+
+-- | Value is @ok@. Lifts it. Works for 'Fallible' and 'Infallible'.
+--
+-- >>> parse (fallible $ ok (star $ try $ is 'a') >> need (is 'b')) "aaab"
+-- Just 'b'
+ok ::
+  (Monoid m, MonadTrans t, Monad (t (Parser m s))) =>
+  Parser m s a ->
+  ExceptT e (t (Parser m s)) a
+ok = lift . lift
+
+-- | Using default value 'e', return early if @Nothing@. Works for
+-- 'Fallible' and 'Infallible'.
+--
+-- >>> parse (infallible $ fallback False (is 'a') >> pure True) "a"
+-- True
+--
+-- >>> parse (infallible $ fallback False (is 'a') >> pure True) "b"
+-- False
+fallback ::
+  (Monoid m, MonadTrans t) =>
+  e ->
+  Parser m s (Maybe a) ->
+  ExceptT e (t (Parser m s)) a
+fallback e = ExceptT . lift . fmap (maybe (Left e) Right)
+
 -- | Parser @need@s a value to return @Just@.
 --
 -- >>> parse (fallible $ need (is 'a') >> need (is 'b') >> pure True) "ab"
@@ -42,22 +67,5 @@ infallible = fmap (either id id) . runIdentityT . runExceptT
 --
 -- >>> parse (fallible $ need (is 'a') >> need (is 'b') >> pure True) "ac"
 -- Nothing
-need :: (Monad m, MonadTrans t) => m (Maybe a) -> t (MaybeT m) a
+need :: Monoid m => Parser m s (Maybe a) -> Fallible e m s a
 need = lift . MaybeT
-
--- | Value is @ok@. Lifts it.
---
--- >>> parse (fallible $ ok (star $ try $ is 'a') >> need (is 'b')) "aaab"
--- Just 'b'
-ok :: (Monad m, Monad (t m), MonadTrans t, MonadTrans u) => m a -> u (t m) a
-ok = lift . lift
-
--- | Using default value 'e', return early if @Nothing@.
---
--- >>> parse (infallible $ fallback False (is 'a') >> pure True) "a"
--- True
---
--- >>> parse (infallible $ fallback False (is 'a') >> pure True) "b"
--- False
-fallback :: (Monad m, MonadTrans t) => e -> m (Maybe a) -> ExceptT e (t m) a
-fallback e = ExceptT . lift . fmap (maybe (Left e) Right)
